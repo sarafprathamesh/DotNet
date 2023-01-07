6e463066f256f46169c6c2d766db659a49dbab30
Sagar Band <sgadave18@gmail.com>
2023-01-07 06:28:56 +0530

    Created New Validation Project wit Serializer and Deserializer

--- Validation/Models/Credential.cs
+++ Validation/Models/Credential.cs
@@ -1,12 +1,15 @@
 namespace UserCredential;
-
-public class Credential{
-    public string UserID{get;set;}
-
-    public string PassWD{get;set;}
-
-    public Credential(string username,string password){
-        this.UserID=username;
-        this.PassWD=password;
+using System.Text.Json.Serialization;
+public class Credential
+{
+    [JsonInclude]
+    public string UserID { get; set; }
+    [JsonInclude]
+    public string PassWD { get; set; }
+    [JsonConstructor]
+    public Credential(string username, string password)
+    {
+        this.UserID = username;
+        this.PassWD = password;
     }
 }
--- Validation/Models/Serialize.cs
+++ Validation/Models/Serialize.cs
@@ -2,56 +2,63 @@ namespace Serialization;
 using UserClass;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Text.Json; 
 using UserCredential;
 
 
 [Serializable]
-public class Serialize
+public static class Serialize
 {
-    private static string userFileNameJSON = @"D:\IACSD PG-DAC Roll No. - 29,60\Assignments\DotNet\Assignments\Validation\wwwroot\database\User.json";
+    private static string userFileNameJSON = @"D:\Github data\Prathamesh Git\DotNet\Validation\wwwroot\database\User.json";
     private static string userJsonString = File.ReadAllText(userFileNameJSON);
-    private static string credentialJSON = @"D:\IACSD PG-DAC Roll No. - 29,60\Assignments\DotNet\Assignments\Validation\wwwroot\database\Credential.json";
-    private static string credentialJsonString = File.ReadAllText(credentialJSON);
+    private static string credentialJSON = @"D:\Github data\Prathamesh Git\DotNet\Validation\wwwroot\database\Credential.json";
+    
+
+
     public static Boolean ValidateUser(string username, string password)
     {
-
-        List<Credential> userList = JsonSerializer.Deserialize<List<Credential>>(credentialJsonString);
+        string credentialJsonString = File.ReadAllText(credentialJSON);
+        // return credentialJsonString;
+var options = new JsonSerializerOptions { IncludeFields = true, PropertyNameCaseInsensitive = true };
+        List<Credential> userList= System.Text.Json.JsonSerializer.Deserialize<List<Credential>>(credentialJsonString,options);
         foreach (Credential user1 in userList)
         {
-            Console.WriteLine("USER ID IN VALIDATOR-----"+user1.UserID);
-            Console.WriteLine("USER PASSWORD IN VALIDATOR-----"+user1.PassWD);
+            Console.WriteLine("USER ID IN VALIDATOR-----" + user1.UserID);
+            Console.WriteLine("USER PASSWORD IN VALIDATOR-----" + user1.PassWD);
             if (user1.UserID == username && user1.PassWD == password)
             {
-                Console.WriteLine("ID---------------------------"+user1.UserID);
-                Console.WriteLine("Password---------------------------"+user1.PassWD);
+                Console.WriteLine("ID---------------------------" + user1.UserID);
+                Console.WriteLine("Password---------------------------" + user1.PassWD);
                 return true;
             }
         }
         return false;
     }
+    
 
     public static bool InsertCredential(string username, string password)
     {
-        var options = new JsonSerializerOptions { IncludeFields = true };
+
+        string credentialJsonString = File.ReadAllText(credentialJSON);
         List<Credential> credentialList = JsonSerializer.Deserialize<List<Credential>>(credentialJsonString);
-       
-       foreach (Credential user2 in credentialList)
+
+        foreach (Credential user2 in credentialList)
         {
             if (user2.UserID == username)
             {
                 return false;
             }
         }
-
-        credentialList.Add(new Credential(username, password));
-        var credential=JsonSerializer.Serialize(credentialList,options);
+        Credential data=new Credential (username, password);
+        credentialList.Add(data);
+        var options = new JsonSerializerOptions { IncludeFields = true };
+        var credential = JsonSerializer.Serialize<List<Credential>>(credentialList, options);
         File.WriteAllText(credentialJSON, credential);
         return true;
     }
 
-    public static bool InsertUserDetails(string username, string firstname,string lastname, string location, string phonenumber ){
+    public static bool InsertUserDetails(string username, string firstname, string lastname, string location, string phonenumber)
+    {
         return false;
     }
 }
